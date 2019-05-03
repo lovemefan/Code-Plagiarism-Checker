426ca454d5db7123c984e9c5fc6955eaa6eff8c4
lovemefan <450489712@qq.com>
2019-05-03 12:33:36 +0800

    word embedding 采用最简单的one hot编码

---
+++ src/CPOC/code-preprocess/word_embedding_one_hot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author :lovemefan
+# @Datetime:2019/5/3 上午9:28
+# @Version: Ubuntu 18.04.2 LTS + python3.6
+# @Software: PyCharm
+
+from keras.preprocessing.text import Tokenizer
+
+
+def text2one_hot_result(samples):
+
+    # i创建一个分词器（tokenizer），设置为只考虑前1000个最常见的单词,足够
+    tokenizer = Tokenizer(num_words=1000)
+
+    tokenizer.fit_on_texts(samples)   # 构建索引单词
+
+    # sequences = tokenizer.texts_to_sequences(samples)   # 将字符串转换为整数索引组成的列表
+
+    one_hot_results = tokenizer.texts_to_matrix(samples, mode='binary')  #可以直接得到one-hot二进制表示。这个分词器也支持除
+
+    # one-hot编码外其他向量化模
+    # word_index = tokenizer.word_index  # 得到单词索引
+    return one_hot_results
+
+
+if __name__ == '__main__':
+    samples = ['The cat cat sat on the mat.', 'The dog ate my homework.']
+    print(text2one_hot_result(samples))
